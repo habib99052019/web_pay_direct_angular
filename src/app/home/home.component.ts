964df41d0cd1851b7315b0670523236a7bb599fa
habib <ha9.0bib90@gmail.com>
2023-11-27 20:54:39 +0100

    o

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser'
+import { Router,ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor(private sanitizer : DomSanitizer) { }
+img ="assets/images/logoVenice.png"
+  constructor(private sanitizer : DomSanitizer, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
   //   const script = document.createElement('script')
   // this.sanitizer.bypassSecurityTrustResourceUrl('../../assets/vendor/global/global.min.js')
   //  document.body.appendChild()
+//   if(this.route.snapshot.paramMap.get('id')==""){
+//   this.img="assets/images/l2.png"
+
+//   }
+//  if(this.route.snapshot.paramMap.get('id')=="WEH - Real Estate"){
+//   this.img="assets/images/l4.png"
+  
+//   }
+
   }
 
 }
